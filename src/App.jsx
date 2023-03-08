902e4706199d5af162613e3cc50c5a8e58e6dba8
Paula0397 <paula230397@gmail.com>
2023-03-07 22:50:28 -0500

    first changes

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Card from "./Components/Card/Card";
+import Form from "./Components/Form/Form";
+import "./App.css";
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Card />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Form/Form.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const Form = () => {
+return (
+    <div><h1>Hola</h1>
+    <form action="">
+        <div>
+            <label htmlFor=""></label>
+            <input type="text" />
+        </div>
+
+
+    </form>
+    
+    </div>
+)
+}
+
+export default Form
